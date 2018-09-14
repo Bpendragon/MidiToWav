b464c8dd1bd8fd950ce8247371eb471f24712300
Bpendragon (David Camp) <david@campclan.net>
2018-09-13 17:55:10 -0700

    flesh out the midistreram a bit

--- MidiToWav/src/MidiStream.cs
+++ MidiToWav/src/MidiStream.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Collections.Generic;
 
 namespace MidiToWav
 {
@@ -19,7 +20,8 @@ namespace MidiToWav
             try
             {
                 dataFile = File.OpenRead(file);
-            } catch
+            }
+            catch
             {
                 throw;
             }
@@ -28,30 +30,69 @@ namespace MidiToWav
         }
     }
 
-    public class HeaderChunk : Chunk 
-    {
-        private MidiFormat Format { get; set; }
-        
-
-
-    }
-
-    public class Chunk
-    {
-        private ChunkType Type { get; set; }
-        private uint Length { get; set; } 
-    }
-
     public enum MidiFormat
     {
         SingleTrack = 0,
         MultiTrack = 1,
         AsyncMultiTrack = 2
-    };
+    }
 
     public enum ChunkType
     {
         Header = 0,
         Track = 1
-    };
+    }
+
+
+    public class HeaderChunk
+    {
+        private MidiFormat Format { get; }
+        private ChunkType Type { get; }
+        private uint Length { get; }
+
+        public HeaderChunk(MidiFormat Format, uint Length)
+        {
+            this.Format = Format;
+            this.Type = ChunkType.Header;
+            this.Length = Length;
+        }
+
+        public HeaderChunk()
+        {
+            this.Type = ChunkType.Header;
+        }
+
+    }
+
+    public class TrackChunk
+    {
+        private ChunkType Type { get; }
+        private uint Length { get; }
+        private List<MidiTrackEvent> Events { get; set; }
+
+    }
+
+    public class MidiTrackEvent
+    {
+        private int DeltaTime { get; }
+        private MidiMessage Message { get; set; }
+
+        public MidiTrackEvent(int DeltaTime, MidiMessage Message)
+        {
+            this.DeltaTime = DeltaTime;
+            this.Message = Message;
+        }
+    }
+
+    public class MidiMessage
+    {
+        private byte Status { get; }
+        private List<byte> Data { get; }
+
+        public MidiMessage(byte Status, List<byte> Data)
+        {
+            this.Status = Status;
+            this.Data = Data;
+        }
+    }
 }
